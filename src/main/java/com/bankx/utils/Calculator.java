e50760c5cd0b77d4ed3a5ec6b0cfff4384a87db6
abhilashmadanu <bala.madanu08@gmail.com>
2023-06-21 13:58:03 -0400

    calculator functionality skeleton

---
+++ src/main/java/com/bankx/utils/Calculator.java
@@ -0,0 +1,11 @@
+package com.bankx.utils;
+
+public class Calculator {
+    public int add(int x, int y){
+        return 0;
+    }
+
+    public int multiply(int x, int y){
+        return 0;
+    }
+}
